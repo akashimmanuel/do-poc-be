8abeed9fb113967cac83dd3f2f091a50fa390843
akashimmanuel <akashimmanuel2@gmail.com>
2022-09-21 14:57:33 +0530

    dry test-1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",
+    async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello World!",
+        });
+    }
+);
+
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+} catch (error) {
+    console.error(`Error occured: ${(error as any)?.message}`);
+}
