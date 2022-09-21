0f42200165f00ef0a1ec90cfd37664858ed6a1ba
akashimmanuel <akashimmanuel2@gmail.com>
2022-09-21 15:56:05 +0530

    dry test 2

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",
+app.get("/home",
     async (req: Request, res: Response): Promise<Response> => {
         return res.status(200).send({
             message: "Hello World!",
